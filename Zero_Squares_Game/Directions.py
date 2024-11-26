cf18650c88038f45647d00654df68d3f2f53d6f1
yamen <yamenalmoghrabi2@gmail.com>
2024-11-25 23:29:31 -0800

    Deleted

--- Zero_Squares_Game/Directions.py
+++
@@ -1,7 +0,0 @@
-
-class Directions :
-    def __init__(self)-> None:
-        self.Directions = ['LEFT' , 'RIGHT' , 'DOWN' , 'UP'] 
-        
-    def Return_Directions(self) :
-        return self.Directions
